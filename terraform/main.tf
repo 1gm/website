412408bf2922822288e76317fc121d4dc1d5ca2c
George Mokbel <georgemokbel@gmail.com>
2019-05-22 23:27:35 -0700

    terraform: Add s3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "mokbel.tfstate"
+    key    = "www.mokbel.io.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
     region = "${var.region}"
 }
